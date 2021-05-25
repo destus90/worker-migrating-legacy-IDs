a407a33f67b8d62ae8879a595fa0b02ed978c24e
pavlenko <pavlenko@technocom.tech>
2021-05-25 11:20:51 +0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'worker-migrating-legacy-IDs';
+
+  ngOnInit() {
+    if (typeof Worker !== 'undefined') {
+      // Create a new
+      const worker = new Worker(new URL('./worker.ts', import.meta.url), { type: 'module' });
+    } else {
+      // Web Workers are not supported in this environment.
+      // You should add a fallback so that your program still executes correctly.
+    }
+  }
 }
